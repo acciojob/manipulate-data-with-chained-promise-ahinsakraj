9068350f0c1779f5623fa21c492169a6887bfaa0
Ahinsak Raj <117904029+ahinsakraj@users.noreply.github.com>
2024-03-02 21:17:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", () => {
+    let arr = [1, 2, 3, 4];
+    let output = document.getElementById("output");
+
+    let promise1 = (inputArry) => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(inputArry);
+            }, 3000);
+        });
+    };
+
+    promise1(arr)
+        .then(filteredData => {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    let evenNumbers = filteredData.filter(item => item % 2 === 0);
+                    output.innerHTML = evenNumbers.join(", ");
+                    resolve(evenNumbers);
+                }, 1000);
+            });
+        })
+        .then(evenNumbers => {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    let multiply = evenNumbers.map(item => item * 2);
+                    output.innerHTML = multiply.join(", ");
+                    resolve(multiply);
+                }, 2000);
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        });
+});
